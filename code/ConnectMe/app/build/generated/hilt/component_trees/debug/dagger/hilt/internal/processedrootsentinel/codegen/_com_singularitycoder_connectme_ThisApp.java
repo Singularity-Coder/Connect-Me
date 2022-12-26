26585484117f0163bf826ef09c934dce5d435efb
Singularity Coder <singularitycoder@192.168.1.2>
2022-12-26 14:17:31 +0530

    removed generated stuff from commit

--- code/ConnectMe/app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_singularitycoder_connectme_ThisApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.singularitycoder.connectme.ThisApp"
-)
-public final class _com_singularitycoder_connectme_ThisApp {
-}
